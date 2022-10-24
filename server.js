92c07e61913a2c86542bd4b350809a01bdbc4321
Reza Saker <saker.reza001@hotmail.com>
2022-10-24 05:06:32 -0500

    Added header

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ app.use(express.urlencoded({ extended: true }));
 
 app.get('/', (req, res) => {
     res.render('index.njk', {
-        username: 'Xyz abc'
+        username: 'Mr. Test'
     });
 });
 
