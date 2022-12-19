9ad2cc7e6ae2210248a976a736baec59c4d657c5
Reza Saker <saker.reza001@hotmail.com>
2022-12-19 14:37:07 -0600

    changes

---
+++ config/config.js
@@ -0,0 +1,13 @@
+const DB = {
+    uri: "mongodb+srv://Student:ACS-3909@cluster0.r974llp.mongodb.net/uwcfos",
+    Id: "gaganrockstar0607@gmail.com",
+    Pass: "Gagan.0607"
+}
+
+const SESSION = {
+    secret: "ksdfhjas889437589kjahfdksdfkyriuowflfkdsfh"
+}
+
+module.exports = {
+    DB, SESSION
+};
